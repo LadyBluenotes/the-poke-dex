f37adefccdb603057bd2e8c7e7bb5f990e1c7474
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-07-06 20:56:27 -0700

    incl changing pages

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,9 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState(['pikachu', 'bulbasaur']);
-  
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [nextPageUrl, setNextPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [prevPageUrl, setPrevPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel()
+  }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
   return (
     <PokemonList pokemon={pokemon} />
   );
