8036dc95cf13c28619c6a17759a3b3946d7f4163
gerrardsarah <gerrardsarah@gmail.com>
2022-07-14 18:44:20 -0700

    styling

--- pokemon-app/src/components/PokemonList.js
+++ pokemon-app/src/components/PokemonList.js
@@ -8,8 +8,16 @@ export default function PokemonList({ pokemon }) {
     return s.charAt(0).toUpperCase() + s.slice(1)
   }
 
+  // when screen width is below 400px display text saying "make screen wider to view content"
+  const displayText = () => {
+    if (window.innerWidth < 400) {
+      return "Make screen wider to view content"
+    }
+  }
+
   return (
     <div className='wrapper'>
+      <p className='textDisplay'>{displayText()}</p>
         {pokemon.map(p => (
           <div key={p[0]}>
             <span>#{p[1].split('/')[6].toString().padStart(3, '0')}<a className='pokemonName' href='#'>{capitalize(p[0])}</a></span>
