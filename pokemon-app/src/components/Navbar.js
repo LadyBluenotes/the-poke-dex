2a493fae4a28d8e8aec6baf5e9daaf69d00e5dd8
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-07-16 16:58:39 -0700

    edited name

--- pokemon-app/src/components/Navbar.js
+++ pokemon-app/src/components/Navbar.js
@@ -6,17 +6,17 @@ function Navbar({}) {
   return (
     <nav className='navigation'>
       <h1 className='title'>PokeDex</h1>
-      <div className='navSearch'>
-        <form>
-          <label>
-            <input
-              type="text"
-              placeholder="Search for a Pokemon"
-            />
-          </label>
-        </form>
-      </div>
-    </nav>
+        <div className='navSearch'>
+          <form>
+            <label>
+              <input
+                type="text"
+                placeholder="Search for a Pokemon"
+              />
+            </label>
+          </form>
+        </div>
+      </nav>
   )
 }
 
