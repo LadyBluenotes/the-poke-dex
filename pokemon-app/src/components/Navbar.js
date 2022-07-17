f3ace6aaa96df2cd453d65ebc059447949a87ac3
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-07-16 17:12:09 -0700

    removed unused packages

--- pokemon-app/src/components/Navbar.js
+++ pokemon-app/src/components/Navbar.js
@@ -3,6 +3,17 @@ import './Navbar.css'
 
 function Navbar({}) {
 
+  // searching for pokemon by name brings up the pokemon's information page, if no pokemon is found, it displays a message saying "No pokemon found"
+  const searchPokemon = (e) => {
+    e.preventDefault()
+    const search = e.target.search.value
+    if (search === '') {
+      alert('Please enter a pokemon name')
+    } else {
+      window.location.href = `/pokemon/${search}`
+    }
+  }
+
   return (
     <nav className='navigation'>
       <h1 className='title'>PokeDex</h1>
@@ -12,6 +23,7 @@ function Navbar({}) {
               <input
                 type="text"
                 placeholder="Search for a Pokemon"
+                onChange={searchPokemon}
               />
             </label>
           </form>
--- pokemon-app/src/components/Pokemon.js
+++ pokemon-app/src/components/Pokemon.js
@@ -18,7 +18,7 @@ export default function PokemonList({ pokemon }) {
       <p className='textDisplay'></p>
         {pokemon.map(p => (
           <div key={p[0]}>
-            <span classname='pokemonIdentifier'>#{p[1].split('/')[6].toString().padStart(3, '0')}<a className='pokemonName' onClick={redirectToPokemon} >{capitalize(p[0])}</a></span>
+            <span className='pokemonIdentifier'>#{p[1].split('/')[6].toString().padStart(3, '0')}<a className='pokemonName' onClick={redirectToPokemon} >{capitalize(p[0])}</a></span>
             <img src={`https://img.pokemondb.net/artwork/large/${p[0]}.jpg`} alt={p[0]} />
             </div>
         ))}
